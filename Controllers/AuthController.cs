6907ed6ef464c4dcfb07fb1d1809fbd705677ba1
ma159123 <ma1980432@gmail.com>
2026-08-25 19:22:58 +0300

    Initial commit: Setup Examination System solution with Clean Architecture

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using Application.CQRS.Features.Auth.Commands.ForgotPassword;
+using Application.CQRS.Features.Auth.Commands.PasswordReset;
+using Application.CQRS.Features.Auth.Commands.Register;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserLoginCommand command)
+        {
+            var result = await _mediator.Send(command);
+            if (result.IsFailure)
+            {
+                return BadRequest(result.Error);
+            }
+            return Ok(result.Value);
+        }
+        [HttpPost("forgot-password")]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordCommand command)
+        {
+            var result = await _mediator.Send(command);
+            if (result.IsFailure)
+            {
+                return BadRequest(result.Error);
+            }
+            return Ok(result);
+        }
+
+        [HttpPost("reset-password")]
+        public async Task<IActionResult> ResetPassword(ResetPasswordCommand command)
+        {
+            var result = await _mediator.Send(command);
+            if (result.IsFailure)
+            {
+                return BadRequest(result.Error);
+            }
+            return Ok(result);
+        }
+    }
+}
